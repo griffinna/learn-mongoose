70dce771bf1406affe6ca54548c4d36f0042712f
Griffin <garam.dev9@gmail.com>
2021-02-15 22:35:43 +0900

    Add: 에러 미들웨어 추가

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/comments', commentsRouter);
 
+app.use((req, res, next) => {
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
